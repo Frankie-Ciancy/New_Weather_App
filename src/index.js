33708868298607de31e872326b963cd3b30da93f
Frankie-Ciancy <francesca.frankie.prina@gmail.com>
2020-07-27 17:03:19 +0200

    Future weather switches months
    Future days now have the correct month and date after the end of the current month

--- src/index.js
+++ src/index.js
@@ -39,7 +39,6 @@ function searchAPI(searchedCity) {
 function displayCurrentTemp(response) {
   let temp = Math.round(response.data.main.temp);
   todayTemp.innerHTML = `${temp}°C`;
-  // chooseUnit();
 }
 function displayCurrentHumidity(response) {
   let humidity = Math.round(response.data.main.humidity);
@@ -89,6 +88,8 @@ function setDateNow() {
   let getYear = now.getFullYear();
   let dateNow = document.querySelector("#date-now");
   dateNow.innerHTML = `${months[getMonth]} ${getDate}, ${getYear}`;
+  displayFuturePlusOne(months, getMonth, getDate, getYear);
+  // checkMonth(monthName, getDate, getYear);
 }
 ////definitions
 
@@ -100,7 +101,7 @@ let unitSwitch = document.querySelector("#unit-switch");
 let apiKey = "d0374efc7dd8f24099f54d563eb3c31c";
 
 ///call functions
-setTodayData();
+
 ////////////////////////////////////////////////////////////////////////////////
 ////click buttons
 discernButton();
@@ -206,6 +207,104 @@ function displayCurrentIcon(response) {
     `<i class="far fa-snowflake"></i>`,
     `<i class="fas fa-smog"></i>`,
   ];
-  console.log(response.data);
   todayIcon.innerHTML = iconSet[iconRetrievalNumber];
 }
+///////// future icons
+
+function checkMonth(months, getMonth, getDate, getYear, previousMonthDays) {
+  let monthName = months[getMonth];
+  let count = 0;
+  let thirtyOneDaysMonths = {
+    January: 0,
+    March: 1,
+    May: 2,
+    July: 3,
+    August: 4,
+    October: 5,
+    December: 6,
+  };
+
+  let thirtyOneDays = Array(32)
+    .fill()
+    .map((x, i) => i);
+
+  let thirtyDaysMonths = ["April", "June", "September", "November"];
+  if (
+    thirtyOneDaysMonths[monthName] !== undefined &&
+    thirtyOneDays[getDate] !== undefined ///defining 31 day months
+  ) {
+  } else if (
+    thirtyDaysMonths[monthName] !== undefined && ////defining thirty day months
+    thirtyOneDays[getDate + 1] !== undefined
+  ) {
+  } else if (
+    getYear / 4 !== undefined &&
+    thirtyOneDays[getDate + 2] !== undefined ///defining february on leap years
+  ) {
+  } else if (thirtyOneDays[getDate + 3] !== undefined) {
+    //// defining february not on leap years
+  } else {
+    monthName = months[getMonth + 1];
+
+    if (
+      thirtyOneDaysMonths[monthName] !== undefined ///defining 31 day months
+    ) {
+      getDate = getDate - 31;
+    } else if (thirtyDaysMonths[monthName] !== undefined) {
+      getDate = getDate - 30;
+    } else if (
+      getYear / 4 !==
+      undefined ///defining february on leap years
+    ) {
+      getDate = getDate - 29;
+    } else {
+      getDate - 28;
+    }
+  }
+  return (futuredate = {
+    date: getDate,
+    month: monthName,
+  });
+}
+
+function dude(count) {
+  count = count + 1;
+  return count;
+}
+
+function displayFuturePlusOne(months, getMonth, getDate, getYear) {
+  let oneDayMore = document.querySelector("#one-day-more");
+  let twoDaysMore = document.querySelector("#two-days-more");
+  let threeDaysMore = document.querySelector("#three-days-more");
+  let fourDaysMore = document.querySelector("#four-days-more");
+  let fiveDaysMore = document.querySelector("#five-days-more");
+  let sixDaysMore = document.querySelector("#six-days-more");
+  let i;
+
+  for (i = 0; i < 6; i++) {
+    let previousMonthDays = i;
+    getDate = getDate + 1;
+    let checkMonthCalls = i;
+    checkMonth(
+      months,
+      getMonth,
+      getDate,
+      getYear,
+      i,
+      checkMonthCalls,
+      previousMonthDays
+    );
+    let futuredisplays = [
+      oneDayMore,
+      twoDaysMore,
+      threeDaysMore,
+      fourDaysMore,
+      fiveDaysMore,
+      sixDaysMore,
+    ];
+
+    futuredisplays[i].innerHTML = `${futuredate.month} ${futuredate.date}`;
+  }
+}
+
+setTodayData();
